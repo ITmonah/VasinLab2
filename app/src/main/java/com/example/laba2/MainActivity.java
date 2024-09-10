4e9d99da2f7d0cdce602b6694ea6e648e2288667
ITmonah <vasin.sasha26@gmail.com>
2024-09-10 11:25:46 +0500

    lab2

---
+++ app/src/main/java/com/example/laba2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.laba2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textRes;
+    private TextView textError;
+    private TextView znak;
+    private EditText inputFirst;
+    private EditText inputSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textRes = findViewById(R.id.textRes);
+        znak = findViewById(R.id.textZnak);
+        textError = findViewById(R.id.textError);
+        inputFirst = findViewById(R.id.InputFirst);
+        inputSecond = findViewById(R.id.InputSecond);
+    }
+
+    public void btnSum(View view) {
+        Number result;
+        if (!inputFirst.getText().toString().equals("") && !inputSecond.getText().toString().equals("")) {
+            result = Double.parseDouble(inputFirst.getText().toString()) + Double.parseDouble(inputSecond.getText().toString());
+            textRes.setText(result.toString());
+            znak.setText("+");
+        } else {
+            textError.setText("Введите числа");
+        }
+    }
+
+    public void btnMin(View view) {
+        Number result;
+        if (!inputFirst.getText().toString().equals("") && !inputSecond.getText().toString().equals("")) {
+            result = Double.parseDouble(inputFirst.getText().toString()) - Double.parseDouble(inputSecond.getText().toString());
+            textRes.setText(result.toString());
+            znak.setText("-");
+        } else {
+            textError.setText("Введите числа");
+        }
+    }
+
+    public void btnDel(View view) {
+        Number result;
+        if (!inputFirst.getText().toString().equals("") && !inputSecond.getText().toString().equals("")) {
+            if (!inputSecond.getText().toString().equals("")) {
+            result = Double.parseDouble(inputFirst.getText().toString()) / Double.parseDouble(inputSecond.getText().toString());
+            textRes.setText(result.toString());
+            znak.setText("/");} else {
+                textError.setText("На ноль делить нельзя");
+            }
+        } else {
+            textError.setText("Введите числа");
+        }
+    }
+
+    public void btnMult(View view) {
+        Number result;
+        if (!inputFirst.getText().toString().equals("") && !inputSecond.getText().toString().equals("")) {
+            result = Double.parseDouble(inputFirst.getText().toString()) * Double.parseDouble(inputSecond.getText().toString());
+            textRes.setText(result.toString());
+            znak.setText("*");
+        } else {
+            textError.setText("Введите числа");
+        }
+    }
+}
